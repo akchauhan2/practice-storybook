90a02966b5774d0a1861e29de5800ed8b58621f4
akchauhan2 <vijayid_94@hotmail.com>
2023-09-10 23:00:26 +0530

    testing bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Open the storybook</div>;
+  return <div className="App">Open the storybook, learn react</div>;
 }
 
 export default App;
